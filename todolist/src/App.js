72e6361ec38b3497271de57223638a10980ffe94
ShreyDhyani <shrey@betalectic.com>
2022-06-14 09:54:53 +0530

    Commit to main branch

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/home/Home.js';
+import Test from './pages/test/Test.js';
 
 export default function App() {
   return (
     <div>
       <Toaster/>
        <Home />
+      {/* <Test/> */}
     </div>
   )
 }
-
---
+++ todolist/src/pages/test/Test.js
@@ -0,0 +1,10 @@
+export default function Test(){
+
+    return(
+        <div className="p-10 bg-yellow-300" >
+            <button className="bg-blue-500 rounded-md ">
+                click me!!
+            </button>
+        </div>
+    )
+}
